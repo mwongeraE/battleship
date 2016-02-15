c821cc2957eea1d6b60505da48e5c9315a6e9840
mwongeraE <evansmwongera@gmail.com>
2016-02-16 00:50:28 +0300

    else

--- battleship.py
+++ battleship.py
@@ -28,3 +28,12 @@ guess_col = int(raw_input("Guess Col:"))
 
 if guess_row == ship_row and guess_col == ship_col:
     print "Congratulations! You sunk my battleship!"
+
+else:
+    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        print "Oops, that's not even in the ocean."
+    elif(board[guess_row][guess_col] == "X"):
+        print "You guessed that one already."
+    else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
