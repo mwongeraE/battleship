057005b1126feefc3afb5b8995a9f98a7890226e
mwongeraE <evansmwongera@gmail.com>
2016-02-16 00:47:59 +0300

    rows and columns

--- battleship.py
+++ battleship.py
@@ -11,3 +11,9 @@ def print_board(board):
 
 print "Let's play Battleship!"
 print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
